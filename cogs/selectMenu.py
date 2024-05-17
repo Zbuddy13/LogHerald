bb40e43c67e6a630ee54729f1776d2671a236f47
Zbuddy13 <jzshaw13@gmail.com>
2024-05-17 19:39:03 -0400

    Update selectMenu.py

--- cogs/selectMenu.py
+++ cogs/selectMenu.py
@@ -3,18 +3,20 @@ import nextcord
 from nextcord.ext import commands
 
 class Menus(nextcord.ui.Select):
-
+    
     # default initialization
     def __init__(self):
-        print("Menus Initialized Successfully")
+        print("Callback Initialized Successfully")
         options = [
             nextcord.SelectOption(label="Python", description="python is cool"),
             nextcord.SelectOption(label="Java", description="java is old")
         ]
+        async def callbackresponse(interaction:Interaction):
+            selected_option = interaction.data.values[0]
+            await interaction.response.send_message(f"You chose option: {selected_option}!")
+
         super().__init__(placeholder="Select your language", min_values=1, max_values=1,options=options)
-        async def callbackresponse(self, interaction:Interaction):
-            await interaction.response.send_message(f'you chose {self.values[0]}')
-        callback = callbackresponse
+        callback = callbackresponse()
 
 class MenuView(nextcord.ui.View):
     def __init__(self):
@@ -28,9 +30,9 @@ class dockerMenu(commands.Cog):
 
     @nextcord.slash_command(name="drop",
                             description="Dropdown test")
-    async def drop(self, interaction: Interaction):
+    async def drop(ctx):
         view = MenuView()
-        await interaction.send("chose an option", view=view)
+        await ctx.send("chose an option", view=view)
 
 
 def setup(client):
