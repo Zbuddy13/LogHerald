51be4b6eaec2c9404230f47c9600e89035849bc6
Zbuddy13 <110738622+Zbuddy13@users.noreply.github.com>
2024-05-17 21:26:13 -0400

    Update selectMenu.py

--- cogs/selectMenu.py
+++ cogs/selectMenu.py
@@ -1,38 +1,53 @@
 from nextcord import Interaction
 import nextcord
 from nextcord.ext import commands
+from nextcord.ui import View, Select
 
-class Menus(nextcord.ui.Select):
+class dockerMenu(Select):
     
     # default initialization
-    def __init__(self):
-        print("Callback Initialized Successfully")
-        options = [
-            nextcord.SelectOption(label="Python", description="python is cool"),
-            nextcord.SelectOption(label="Java", description="java is old")
-        ]
-        async def callbackresponse(interaction:Interaction):
-            selected_option = interaction.data.values[0]
-            await interaction.response.send_message(f"You chose option: {selected_option}!")
-
-        super().__init__(placeholder="Select your language", min_values=1, max_values=1,options=options)
-        callback = callbackresponse()
-
-class MenuView(nextcord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(Menus())
-
-class dockerMenu(commands.Cog):
     def __init__(self, client):
         print("DockerMenu Initialized Successfully")
         self.client = client
 
-    @nextcord.slash_command(name="drop",
+        @nextcord.slash_command(name="drop",
                             description="Dropdown test")
-    async def drop(self, interaction:Interaction):
-        view = MenuView()
-        await interaction.response.send("chose an option", view=view)
+        
+        async def drop(self, interaction:Interaction):
+
+            async def callbackresponse(interaction:Interaction):
+                selected_option = interaction.data.values[0]
+                await interaction.response.send_message(f"You chose option: {selected_option}!")
+            
+            options = [
+                nextcord.SelectOption(label="Python", description="python is cool"),
+                nextcord.SelectOption(label="Java", description="java is old")
+            ]
+
+            dropdown = nextcord.Select(placeholder="What docker container would you like to select?", 
+                                       options=options,  max_values=1, min_values=1)
+            
+            dropdown.callback = callbackresponse
+            myview = View(timeout=180)
+            myview.add_item(dropdown)
+            await interaction.send('Hello!', view=myview)
+            
+
+# class MenuView(nextcord.ui.View):
+#     def __init__(self):
+#         super().__init__()
+#         self.add_item(Menus())
+
+# class dockerMenu(commands.Cog):
+#     def __init__(self, client):
+#         print("DockerMenu Initialized Successfully")
+#         self.client = client
+
+#     @nextcord.slash_command(name="drop",
+#                             description="Dropdown test")
+#     async def drop(self, interaction:Interaction):
+#         view = MenuView()
+#         await interaction.response.send("chose an option", view=view)
 
 
 def setup(client):
